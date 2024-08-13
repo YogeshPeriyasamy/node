0844be6e0f3281871ecb201249941792b16cb0cd
Yogeshperiyasamy <yogeshsri1209@gmail.com>
2024-08-13 12:28:49 +0530

    the first responsive node server

--- firstserver.js
+++ firstserver.js
@@ -1,8 +1,13 @@
-const http=require("http");
+const ahttp=require("http");
 function serverprovider(req,res)
 {
-    console.log("Yogesh");
+    res.setHeader('Content-Type','text/html')
+    res.write('<html>')
+    res.write('<head><title>node server</title></head>');
+    res.write('<body><h1>Node server has been created</h1></body>')
+    res.write('</html>')
+    res.end()
 }
 
-const server=http.createServer(serverprovider)
-server.listen(4000);
+const server=ahttp.createServer(serverprovider)
+server.listen(3000);
