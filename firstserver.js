1d04a1b15852ea44e83837311807a61f0210c609
Yogeshperiyasamy <yogeshsri1209@gmail.com>
2024-08-13 20:35:30 +0530

    url request understanding

--- firstserver.js
+++ firstserver.js
@@ -1,10 +1,21 @@
 const ahttp=require("http");
 function serverprovider(req,res)
 {
+    //console.log(req);
     res.setHeader('Content-Type','text/html')
     res.write('<html>')
     res.write('<head><title>node server</title></head>');
-    res.write('<body><h1>Node server has been created</h1></body>')
+    res.write('<body>');
+    if(req.url=='/home'){
+        res.write('<h1> Welcome home</h1>')
+    }
+    else if(req.url=='/about'){
+        res.write('<h1> Welcome to About Us page</h1>')
+    }
+    else{
+        res.write('<h1>  Welcome to my Node Js project</h1>')
+    }
+    res.write('</body>');
     res.write('</html>')
     res.end()
 }
